05e6a8e9e0003ec816fc6fcedc904336c40d253a
Clément Rigo <rigoclement@mydnic.be>
2015-10-12 17:33:50 +0200

    Icecast sometimes returns decoded HTML

--- fetcher.php
+++ fetcher.php
@@ -2,4 +2,4 @@
 header('Content-Type: text/html; charset=UTF-8');
 $html = file_get_contents($_GET['host']);
 preg_match('$Currently playing:</td><td class="streamstats">(.*)</td>$s',$html,$match);
-echo preg_replace('/\s+/', ' ',$match[1]);
+echo htmlspecialchars_decode(preg_replace('/\s+/', ' ',$match[1]));
